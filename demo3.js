55f6e30fb846df9f724590cf666541e3ba9fe49a
JASMINE BROWN <jasmineiona@JASMINEs-MacBook-Pro.local>
2015-09-30 15:19:13 -0700

    second commit js file

---
+++ demo3.js
@@ -0,0 +1,16 @@
+var question1 = prompt("Was it easy to get to Code Fellows today?");
+
+if (question1.toUpperCase() === 'YES') {
+	alert('Lucky you');
+} else {
+	alert('Sucks to be you');
+}
+
+console.log('Exactly what the user entered: ' + question1);
+console.log('What I am testing in my "if" condition: ' + question1.toUpperCase(
+	));
+
+// here are some changes...
+
+
+
