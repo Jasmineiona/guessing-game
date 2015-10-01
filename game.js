d0195d147de694ee1098d38e8ef11e510a95cf37
Jasmine Iona Brown <jasmineiona@hotmail.com>
2015-09-30 23:29:23 -0700

    update game.js
    3 questions

--- game.js
+++ game.js
@@ -6,11 +6,35 @@ if (question1.toUpperCase() === 'YES') {
 	alert('Sucks to be you');
 }
 
+var question2 = prompt("Where are you communting from?");
+
+if (question2.toUpperCase() === 'Tacoma') {
+	alert('That is it');
+} else {
+	alert('Nope');
+}
+
+var question3 = prompt("Did you make it to class on time?");
+
+if (question3.toUpperCase() === 'YES') {
+	alert('Good Job');
+} else {
+	alert('Better luck tomorrow');
+}
+
 console.log('Exactly what the user entered: ' + question1);
 console.log('What I am testing in my "if" condition: ' + question1.toUpperCase(
 	));
 
-// here are some changes...
+console.log('Exactly what the user entered: ' + question2);
+console.log('What I am testing in my "if" condition: ' + question2.toUpperCase(
+	));
 
+console.log('Exactly what the user entered: ' + question3);
+console.log('What I am testing in my "if" condition: ' + question3.toUpperCase(
+	));
+
+
+// here are some changes...
 
 
